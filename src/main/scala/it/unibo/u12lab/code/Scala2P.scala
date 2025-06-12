995ba2a5df7012cf9c90bdb7987f304053957fcb
g-mainardi <giosue.mainardi@studio.unibo.it>
2025-06-12 10:55:45 +0200

    Ex 4 - Refactor and improve Scala2Prolog integration (theory from file, stream of solutions, term list)

--- src/main/scala/it/unibo/u12lab/code/Scala2P.scala
+++ src/main/scala/it/unibo/u12lab/code/Scala2P.scala
@@ -2,17 +2,28 @@ package it.unibo.u12lab.code
 
 import alice.tuprolog.*
 
+import scala.io.Source
+
 object Scala2P:
+  def extractTerm(solveInfo: SolveInfo, i: Integer): Term =
+    (solveInfo.getSolution.asInstanceOf[Struct] getArg i).getTerm
 
-  def extractTerm(solveInfo:SolveInfo, i:Integer): Term =
-    solveInfo.getSolution.asInstanceOf[Struct].getArg(i).getTerm
+  def extractTerm(solveInfo: SolveInfo, s: String): Term =
+    solveInfo getTerm s
 
-  def extractTerm(solveInfo:SolveInfo, s:String): Term =
-    solveInfo.getTerm(s)
+  def extractListFromTerm(t: Term): LazyList[String] = t match
+    case struct: Struct if struct.getName == "." =>
+      LazyList((struct getArg 0).toString) ++ extractListFromTerm(struct getArg 1)
+    case v: Var => extractListFromTerm(v.getTerm)
+    case _      => LazyList.empty
 
-  given Conversion[String, Term] = Term.createTerm(_)
-  given Conversion[Seq[_], Term] =  _.mkString("[",",","]")
-  given Conversion[String, Theory] = new Theory(_);
+  extension (solutions: LazyList[SolveInfo])
+    def extractSolutionsOf(target: String): LazyList[String] = solutions map (extractTerm(_, s"$target"))
+
+  given Conversion[String, Term]   = Term.createTerm(_)
+  given Conversion[Term, String]   = _.toString
+  given Conversion[Seq[_], Term]   = _.mkString("[", ",", "]")
+  given Conversion[String, Theory] = new Theory(_)
 
   def mkPrologEngine(theory: Theory): Term => LazyList[SolveInfo] =
     val engine = Prolog()
@@ -20,25 +31,32 @@ object Scala2P:
 
     goal => new Iterable[SolveInfo]{
 
-      override def iterator = new Iterator[SolveInfo]{
-        var solution: Option[SolveInfo] = Some(engine.solve(goal))
+      override def iterator: Iterator[SolveInfo] = new Iterator[SolveInfo] {
+        var solution: Option[SolveInfo] = Some(engine solve goal)
 
-        override def hasNext = solution.isDefined &&
+        override def hasNext: Boolean = solution.isDefined &&
                               (solution.get.isSuccess || solution.get.hasOpenAlternatives)
 
-        override def next() =
+        override def next(): SolveInfo =
           try solution.get
-          finally solution = if (solution.get.hasOpenAlternatives) Some(engine.solveNext()) else None
+          finally solution = if solution.get.hasOpenAlternatives then Some(engine.solveNext()) else None
       }
-    }.to(LazyList)
-
+    } to LazyList
 
   def solveWithSuccess(engine: Term => LazyList[SolveInfo], goal: Term): Boolean =
-    engine(goal).map(_.isSuccess).headOption == Some(true)
+    engine(goal).map(_.isSuccess).headOption contains true
 
   def solveOneAndGetTerm(engine: Term => LazyList[SolveInfo], goal: Term, term: String): Term =
-    engine(goal).headOption.map(extractTerm(_,term)).get
+    engine(goal).headOption.map(extractTerm(_, term)).get
 
+  def loadPrologTheory(path: String): String =
+    try
+      val source = Source fromFile path
+      val content = source.mkString
+      source.close()
+      content
+    catch
+      case e: Exception => println(s"Errore nel caricamento del file $path: ${e.getMessage}"); throw e
 
 object TryScala2P extends App:
   import Scala2P.{*, given}
@@ -50,7 +68,7 @@ object TryScala2P extends App:
     permutation(L,[H|TP]) :- member(L,H,T), permutation(T,TP).
   """)
 
-  engine("permutation([1,2,3],L)") foreach (println(_))
+  engine("permutation([1,2,3],L)") foreach println
   // permutation([1,2,3],[1,2,3]) ... permutation([1,2,3],[3,2,1])
 
   val input = Struct("permutation",(1 to 20), Var())
