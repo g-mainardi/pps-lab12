f008533dd25fd540624aee9c472aa3f020cdd6fa
g-mainardi <giosue.mainardi@studio.unibo.it>
2025-06-12 11:43:00 +0200

    More comments on prolog planner

--- src/main/prolog/planner.pl
+++ src/main/prolog/planner.pl
@@ -1,4 +1,4 @@
-% Ex 4.1
+% Ex 4.1 - Basic planner
 % mine predicates for expansion
 gridbound(3).
 
@@ -19,28 +19,35 @@ plan(_, [], P):- goal(P), !.
 plan(0, [], _):- !, fail.
 plan(N, [Cmd|T], P):- move(Cmd, P, Pn), Nn is N - 1, plan(Nn, T, Pn).
 
+% Ex 4.2 - Improvements
+
 % validcoord(?Coordinate)
+% => {0,1,2,3}
 validcoord(C):- validcoord(0, C).
 validcoord(A, A).
 validcoord(A, X):- gridbound(B), A < B, A2 is A + 1, validcoord(A2, X).
 
 % validpos(?Pos)
+% => {s(0,0), s(0,1), ..., s(3,3)}
 validpos(s(X, Y)):- validcoord(X), validcoord(Y).
 
 % freepos(?Pos)
+% => {s(0,1), s(0,2), ..., s(3,2)}
 freepos(P):- validpos(P),	not(init(P)), not(goal(P)).
 
-% utils to check numbers
+% utils to check numbers (not fully relational)
 even(0).
 even(N):- number(N), N > 1, N1 is N - 2, even(N1), !.
 odd(N):- 	number(N), N > 0, not(even(N)).
 
 % loose(+Pos)	
+% => {s(0, 1), s(0, 3), s(2,1), s(2,3)}
 loose(P):-
 	freepos(P), copy_term(P, s(X, Y)),
 	even(X), odd(Y). % losing conditions
 
 % trampoline(+Pos, -NewPos)
+% => Pos in {s(1, 3), s(3, 1)}
 % In determined positions, the robot will jump back in X or Y of 2
 trampoline(P, Pn):-
 	freepos(P), not(loose(P)), copy_term(P, s(X, Y)),
@@ -48,6 +55,7 @@ trampoline(P, Pn):-
 	back(P, Pn).
 
 % back(+Pos, -NewPos)
+% => back(s(1,3), s(1,1))
 % A position with a new one with X or Y diminished by 2
 back(s(X, Y), P):- validcoord(X), Xn is X - 2, P = s(Xn, Y),	validpos(P).
 back(s(X, Y), P):- validcoord(Y), Yn is Y - 2, P = s(X, Yn),	validpos(P).
@@ -56,6 +64,7 @@ back(s(X, Y), P):- validcoord(Y), Yn is Y - 2, P = s(X, Yn),	validpos(P).
 bonus(1).
 
 % jump(?Dir, @Pos, ?NewPos)
+% => Get commands that rapresents using move two times
 jump(up2   , P, P2):- move(up, P, P1)   , move(up, P1, P2).
 jump(down2 , P, P2):- move(down, P, P1) , move(down, P1, P2).
 jump(left2 , P, P2):- move(left, P, P1) , move(left, P1, P2).
